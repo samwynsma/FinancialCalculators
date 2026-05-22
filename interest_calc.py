afe9745886aa617febe179bb88b8444ae5430946
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-05-22 13:15:08 -0700

    Move document generation to new function.
    Moves excel document creation to a different method than the inputs.

--- interest_calc.py
+++ interest_calc.py
@@ -5,7 +5,6 @@ def generate_interest():
     starting_value = -1.0
     interest_rate = -1.0
     time = -1
-    money_by_year = []
     while starting_value < 0.0:
         num_input = input("How much money are we starting with: ")
         try:
@@ -25,30 +24,39 @@ def generate_interest():
         except:
             print("Invalid input: try putting a number there")
 
-    money_by_year.append(starting_value)
-    current_money = starting_value
-    for i in range(time):
-        current_money = round(current_money * (1 + interest_rate / 100), 2)
-        money_by_year.append(current_money)
+    generate_interest_document(starting_value, interest_rate, time)
+    return
+
+def generate_interest_document(start, interest, periods, duration_of_period="Year", additional_money=0.0):
+    money_by_period = []
+    money_by_period.append(start)
+
+    if(duration_of_period == "month"):
+        interest = interest / 12
+        duration_of_period = "Month"
+    else:
+        duration_of_period = "Year"
+
+    current_money = start
+    for i in range(periods):
+        current_money = round(current_money * (1 + interest / 100), 2) + additional_money
+        money_by_period.append(current_money)
     
 
     workbook = xlsxwriter.Workbook("InterestCalculation.xlsx", {'strings_to_numbers' : True})
     worksheet = workbook.add_worksheet("Money with Interest")
 
-    worksheet.write("A1", "Budget calculation at %d percent interest" % (interest_rate))
-    worksheet.write("A2", "Year")
-    worksheet.write("B2", "Money at Year")
+    worksheet.write("A1", "Budget calculation at %d percent interest" % (interest))
+    worksheet.write("A2", duration_of_period)
+    worksheet.write("B2", "Money at %s" % (duration_of_period))
     money_format = workbook.add_format({'num_format': '$#,##0.00'})
 
 
-    for i in range(time + 1):
-        print("Money at the beginning of year %d: %d" % (i, money_by_year[i]))
+    for i in range(periods + 1):
+        print("Money at the beginning of year %d: %d" % (i, money_by_period[i]))
         worksheet.write(i+2, 0, i)
-        worksheet.write(i+2, 1, money_by_year[i], money_format)
+        worksheet.write(i+2, 1, money_by_period[i], money_format)
 
     workbook.close()
     print("Finished creating financial documents.")
-    return
-
-def generate_interest_document():
     return
