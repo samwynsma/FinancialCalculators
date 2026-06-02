df441873d66605999070497abde6dda858ef635a
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-06-02 14:43:34 -0700

    Adding the basis for budget calculator.

---
+++ budget_calc.py
@@ -0,0 +1,10 @@
+def budget_maker(fileExists):
+    print("Welcome to the budget calculator. I'm going to ask you some questions about your life to determine your budget.")
+    print("At the end, we will see how much leftover money there is, and I will give some recommendations.")
+    people = 0
+    pets = 0
+    mortgage_rent = 0.0
+    debt_payments = 0.0
+    medical = 0.0
+
+    return fileExists
--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+from budget_calc import budget_maker
 from interest_calc import generate_interest
 from loans_calc import loan_payoff
 from retirement_goal_calc import retirement_goal
@@ -6,13 +7,15 @@ def main():
     menu_choice = ""
     excelFileExists = False
     while menu_choice != "quit" and menu_choice != "q":
-        menu_choice = input("Welcome to Sam's financial planning application. Choose one of the options below. To close the application, type \"quit\" \n 1. Investments \n 2. Loan Payoff Time \n 3. Retirement Goals Calculator ").lower()
+        menu_choice = input("Welcome to Sam's financial planning application. Choose one of the options below. To close the application, type \"quit\" \n 1. Investments \n 2. Loan Payoff Time \n 3. Retirement Goals Calculator \n 4. Budget Calculator ").lower()
         if(menu_choice == "1" or menu_choice == "investments"):
             excelFileExists = generate_interest(excelFileExists)
         elif(menu_choice == "2" or menu_choice == "loan payoff time"):
             excelFileExists = loan_payoff(excelFileExists)
         elif(menu_choice == "3" or menu_choice == "retirement goals"):
             excelFileExists = retirement_goal(excelFileExists)
+        elif(menu_choice == "4" or menu_choice == "budget maker"):
+            excelFileExists = budget_maker(excelFileExists)
         
 
 if __name__ == "__main__":
