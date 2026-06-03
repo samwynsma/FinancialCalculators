4d1dca4867b83cceb7023f53f527cf2371755164
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-06-03 13:35:21 -0700

    Adds expenditures information and begins the prompts.

--- budget_calc.py
+++ budget_calc.py
@@ -1,10 +1,44 @@
 def budget_maker(fileExists):
     print("Welcome to the budget calculator. I'm going to ask you some questions about your life to determine your budget.")
+    print("Furthermore, I'm going to ask what your current spending is, so we can do a side by side comparison.")
     print("At the end, we will see how much leftover money there is, and I will give some recommendations.")
     people = 0
     pets = 0
+    cars = 0
     mortgage_rent = 0.0
+    auto_payments = 0.0
     debt_payments = 0.0
+    utilities = 0.0
+    insurance = 0.0
+    food = 0.0
+    extra_expenditures = 0.0
     medical = 0.0
+
+    while(people <= 0):
+        num_input = input("How many people are in your family? ")
+        try:
+            people = int(num_input)
+            if people <= 0:
+                print("Invalid input: you count as one of those people.")
+        except:
+            print("Invalid input: try putting a number there")
+    
+    while(pets < 0):
+        num_input = input("How many pets do you have? ")
+        try:
+            pets = int(num_input)
+            if pets < 0:
+                print("Invalid input: you cannot have a negative number of pets.")
+        except:
+            print("Invalid input: try putting a number there")
+    
+    while(cars < 0):
+        num_input = input("How many people are in your family? ")
+        try:
+            cars = int(num_input)
+            if cars < 0:
+                print("Invalid input: you cannot have a negative number of cars.")
+        except:
+            print("Invalid input: try putting a number there")
 
     return fileExists
