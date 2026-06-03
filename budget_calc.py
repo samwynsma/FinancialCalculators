ae5b3aa851cf1260d62fe38808ae59b854c86623
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-06-03 14:01:57 -0700

    Added the rest of the budget calc.

--- budget_calc.py
+++ budget_calc.py
@@ -2,6 +2,7 @@ def budget_maker(fileExists):
     print("Welcome to the budget calculator. I'm going to ask you some questions about your life to determine your budget.")
     print("Furthermore, I'm going to ask what your current spending is, so we can do a side by side comparison.")
     print("At the end, we will see how much leftover money there is, and I will give some recommendations.")
+    monthly_pay = 0.0
     people = 0
     pets = 0
     cars = 0
@@ -15,6 +16,16 @@ def budget_maker(fileExists):
     medical = 0.0
     giving = 0.0
     investments = 0.0
+
+
+    while(monthly_pay <= 0.0):
+        num_input = input("How much money do you take home each month? This is after taxes, 401k, etc. ")
+        try:
+            monthly_pay = int(num_input)
+            if(monthly_pay <= 0):
+                print("I'm not doing this if you don't take home any monthly pay.")
+        except:
+            print("Invalid input: try putting a number there")
 
     while(people <= 0):
         num_input = input("How many people are in your family? ")
@@ -73,8 +84,8 @@ def budget_maker(fileExists):
     while utilities < 0.0:
         num_input = input("How much do you pay on utilities every month? ")
         try:
-            debt_payments = float(num_input)
-            if debt_payments < 0.0:
+            utilities = float(num_input)
+            if utilities < 0.0:
                 print("Invalid input: utilities cannot be negative.")
         except:
             print("Invalid input: try putting a number there")
@@ -82,8 +93,8 @@ def budget_maker(fileExists):
     while insurance < 0.0:
         num_input = input("How much do you pay on insurance (medical, pet, auto, home, life) every month? ")
         try:
-            debt_payments = float(num_input)
-            if debt_payments < 0.0:
+            insurance = float(num_input)
+            if insurance < 0.0:
                 print("Invalid input: insurance cannot be negative.")
         except:
             print("Invalid input: try putting a number there")
@@ -94,6 +105,42 @@ def budget_maker(fileExists):
             food = float(num_input)
             if food <= 0.0:
                 print("Invalid input: You must spend money on food.")
+        except:
+            print("Invalid input: try putting a number there")
+    
+    while medical < 0.0:
+        num_input = input("Do you have any medical co-pays?")
+        try:
+            medical = float(num_input)
+            if medical < 0.0:
+                print("Invalid input: medical copays cannot be negative.")
+        except:
+            print("Invalid input: try putting a number there")
+    
+    while giving < 0.0:
+        num_input = input("How much money do you give every month? ")
+        try:
+            giving = float(num_input)
+            if giving < 0.0:
+                print("Invalid input: giving cannot be negative.")
+        except:
+            print("Invalid input: try putting a number there")
+    
+    while investments < 0.0:
+        num_input = input("How much money do you give every month? ")
+        try:
+            investments = float(num_input)
+            if investments < 0.0:
+                print("Invalid input: investments cannot be negative.")
+        except:
+            print("Invalid input: try putting a number there")
+    
+    while extra_expenditures < 0.0:
+        num_input = input("How much money do you spend on extra expenditures beyond those listed here? ")
+        try:
+            extra_expenditures = float(num_input)
+            if extra_expenditures < 0.0:
+                print("Invalid input: extra expenditures cannot be negative.")
         except:
             print("Invalid input: try putting a number there")
 
