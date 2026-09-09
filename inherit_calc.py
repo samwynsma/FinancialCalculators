bc9c71fe5fb904e3980f4505c3ee112ed8e3f980
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-09-08 19:52:44 -0700

    Adds page info to inherit calc.

--- inherit_calc.py
+++ inherit_calc.py
@@ -206,8 +206,12 @@ class InheritanceCalculator:
                 except:
                     pass
             worksheet.column_dimensions[column].width = length + 2
-        
 
+        
+        page_info = []
+        for row in worksheet.iter_rows(values_only=True):
+            page_info.append(list(row))
+        self.document.create_page(page_info)
         workbook.save(self.excel_file)
         print("Finished creating inheritance documents.")
         return self.file_exists
