b3f8cc11b7eb31f148268e6d54379c76ac26df25
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-09-16 19:11:31 -0700

    Adds document to food_calc.

--- food_calc.py
+++ food_calc.py
@@ -232,8 +232,11 @@ class FoodCostCalculator:
                 except:
                     pass
             worksheet.column_dimensions[column].width = length + 2
-        
 
+        page_info = []
+        for row in worksheet.iter_rows(values_only=True):
+            page_info.append(list(row))
+        self.document.create_page(page_info)
         workbook.save(self.excel_file)
         print("Finished creating food cost documents.")
         return self.file_exists
