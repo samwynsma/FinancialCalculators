23d0b008404a733e594401947bf76fa627004a8c
samwynsma <cpcs.samuel.wynsma@gmail.com>
2026-09-17 18:38:16 -0700

    Adds self document to loan calc.

--- loans_calc.py
+++ loans_calc.py
@@ -183,6 +183,10 @@ class LoanCalculator:
                     pass
             worksheet.column_dimensions[column].width = length + 2
 
+        page_info = []
+        for row in worksheet.iter_rows(values_only=True):
+            page_info.append(list(row))
+        self.document.create_page(page_info)
         workbook.save(self.excel_file)
         return self.file_exists
 
